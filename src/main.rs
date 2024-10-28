00d68c92e5947988403a619bb6429cb2d9e8259a
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-10-28 13:30:43 +0100

    load kick sample

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+#![allow(unused)]
+
+use std::fs;
+use std::path::Path;
+use std::sync::Arc;
+
+const WAV_PATH_909: &'static str = "./samples/909/";
+
 fn main() {
-    println!("Hello, world!");
+    let path = Path::new(WAV_PATH_909);
+    let path = path.join("kick.wav");
+    let kick: Arc<[u8]> = fs::read(path).unwrap().into();
 }
